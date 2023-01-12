d3df0547bdbe7cb79c78d03af44a94ff845369ab
Michael A Manzano <michael.a.manzano@intel.com>
2023-01-12 11:01:56 -0700

    no module federation on shared

--- shared/config/webpack.common.js
+++ shared/config/webpack.common.js
@@ -23,6 +23,8 @@ module.exports = {
     //https://webpack.js.org/configuration/resolve/#resolveextensions
     // webpack by itself resolves js/json/wasm files but if you override it like
     // we do you need to specify "..." at the end to bring defaults back
+    // On a file like FruitButton.tsx where it imports 'FruitButton.scss' its
+    // better to include the file extensions so webpack explicitly knows how to resolve
     extensions: [".tsx", ".ts", "..."],
   },
 };
--- shared/config/webpack.dev.js
+++ shared/config/webpack.dev.js
@@ -1,8 +1,6 @@
 const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const commonConfig = require("./webpack.common");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const { dependencies } = require("../package.json");
 
 const devConfig = {
   mode: "development",
@@ -11,25 +9,6 @@ const devConfig = {
     historyApiFallback: true,
   },
   plugins: [
-    new ModuleFederationPlugin({
-      name: "Shared",
-      filename: "remoteEntry.js",
-      exposes: {
-        "./Styles": "./src/styles/Global",
-        "./Components": "./src/components/.Components",
-      },
-      shared: {
-        ...dependencies,
-        react: {
-          singleton: true,
-          requiredVersion: dependencies["react"],
-        },
-        "react-dom": {
-          singleton: true,
-          requiredVersion: dependencies["react-dom"],
-        },
-      },
-    }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
